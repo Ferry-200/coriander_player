e6c7ffeedea3475a83cb4cc0e445d16a9f1a36d6
Ferry <biyanci1@outlook.com>
2024-03-15 18:15:51 +0800

    逐字歌词绘制支持

---
+++ lib/page/now_playing_page/component/lyric_view_tile.dart
@@ -0,0 +1,308 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:coriander_player/lyric/lrc.dart';
+import 'package:coriander_player/lyric/lyric.dart';
+import 'package:coriander_player/play_service.dart';
+import 'package:coriander_player/theme/theme_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:provider/provider.dart';
+
+class LyricViewTile extends StatelessWidget {
+  const LyricViewTile(
+      {super.key, required this.line, required this.opacity, this.onTap});
+
+  final LyricLine line;
+  final double opacity;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Provider.of<ThemeProvider>(context);
+
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Opacity(
+        opacity: opacity,
+        child: InkWell(
+          onTap: onTap,
+          borderRadius: BorderRadius.circular(12.0),
+          hoverColor: theme.palette.onSecondaryContainer.withOpacity(0.08),
+          highlightColor: theme.palette.onSecondaryContainer.withOpacity(0.12),
+          splashColor: theme.palette.onSecondaryContainer.withOpacity(0.12),
+          child: line is SyncLyricLine
+              ? _SyncLineContent(
+                  syncLine: line as SyncLyricLine,
+                  isMainLine: opacity == 1.0,
+                )
+              : _LrcLineContent(
+                  lrcLine: line as LrcLine,
+                  isMainLine: opacity == 1.0,
+                ),
+        ),
+      ),
+    );
+  }
+}
+
+class _SyncLineContent extends StatelessWidget {
+  const _SyncLineContent(
+      {super.key, required this.syncLine, required this.isMainLine});
+
+  final SyncLyricLine syncLine;
+  final bool isMainLine;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Provider.of<ThemeProvider>(context);
+
+    if (!isMainLine) {
+      final List<Text> contents = [
+        buildText(syncLine.content, theme),
+      ];
+      if (syncLine.translation != null) {
+        contents.add(buildText(syncLine.translation!, theme));
+      }
+
+      return Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: contents,
+        ),
+      );
+    }
+
+    final List<Widget> contents = [
+      StreamBuilder(
+        stream: PlayService.instance.positionStream,
+        builder: (context, snapshot) {
+          final posInMs = (snapshot.data ?? 0) * 1000;
+          final posFromLineStart = max(
+            posInMs - syncLine.start.inMilliseconds,
+            0,
+          );
+          return Wrap(
+            children: List.generate(
+              syncLine.words.length,
+              (i) {
+                final posFromWord = max(
+                  posFromLineStart - syncLine.words[i].start.inMilliseconds,
+                  0,
+                );
+                final progress = min(
+                  posFromWord / syncLine.words[i].length.inMilliseconds,
+                  1.0,
+                );
+                return ShaderMask(
+                  blendMode: BlendMode.dstIn,
+                  shaderCallback: (bounds) {
+                    return LinearGradient(
+                      colors: [
+                        theme.palette.primary,
+                        theme.palette.primary,
+                        theme.palette.onSecondaryContainer.withOpacity(0.15),
+                        theme.palette.onSecondaryContainer.withOpacity(0.15),
+                      ],
+                      stops: [0, progress, progress, 1],
+                    ).createShader(bounds);
+                  },
+                  child: Text(
+                    syncLine.words[i].content,
+                    style: TextStyle(
+                      color: theme.palette.primary,
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      )
+    ];
+    if (syncLine.translation != null) {
+      contents.add(buildText(syncLine.translation!, theme));
+    }
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: contents,
+      ),
+    );
+  }
+
+  Text buildText(String text, ThemeProvider theme) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: theme.palette.onSecondaryContainer,
+        fontSize: 22.0,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+}
+
+class _LrcLineContent extends StatelessWidget {
+  const _LrcLineContent(
+      {super.key, required this.lrcLine, required this.isMainLine});
+
+  final LrcLine lrcLine;
+  final bool isMainLine;
+
+  @override
+  Widget build(BuildContext context) {
+    if (lrcLine.isBlank) {
+      if (lrcLine.length > const Duration(seconds: 5) && isMainLine) {
+        return LrcTransitionTile(line: lrcLine);
+      } else {
+        return const SizedBox.shrink();
+      }
+    }
+
+    final theme = Provider.of<ThemeProvider>(context);
+
+    final splited = lrcLine.content.split("┃");
+    final List<Text> contents = [
+      buildText(splited.first, theme),
+    ];
+    for (var i = 1; i < splited.length; i++) {
+      contents.add(buildText(splited[i], theme));
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: contents,
+      ),
+    );
+  }
+
+  Text buildText(String text, ThemeProvider theme) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: theme.palette.onSecondaryContainer,
+        fontSize: 22.0,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+}
+
+/// Lrc歌词间奏表示
+class LrcTransitionTile extends StatelessWidget {
+  final LrcLine line;
+  const LrcTransitionTile({super.key, required this.line});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Provider.of<ThemeProvider>(context);
+
+    return SizedBox(
+      height: 40.0,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(12, 18, 12, 6),
+        child: CustomPaint(
+          painter: LrcTransitionPainter(
+            theme,
+            LrcTransitionTileController(line),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LrcTransitionPainter extends CustomPainter {
+  final ThemeProvider theme;
+  final LrcTransitionTileController controller;
+
+  final Paint circlePaint1 = Paint();
+  final Paint circlePaint2 = Paint();
+  final Paint circlePaint3 = Paint();
+
+  final double radius = 6;
+
+  LrcTransitionPainter(this.theme, this.controller)
+      : super(repaint: controller);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    circlePaint1.color = theme.palette.onSecondaryContainer.withOpacity(
+      0.05 + min(controller.progress * 3, 1) * 0.95,
+    );
+    circlePaint2.color = theme.palette.onSecondaryContainer.withOpacity(
+      0.05 + min(max(controller.progress - 1 / 3, 0) * 3, 1) * 0.95,
+    );
+    circlePaint3.color = theme.palette.onSecondaryContainer.withOpacity(
+      0.05 + min(max(controller.progress - 2 / 3, 0) * 3, 1) * 0.95,
+    );
+
+    final rWithFactor = radius + controller.sizeFactor;
+    final c1 = Offset(rWithFactor, 8);
+    final c2 = Offset(4 * rWithFactor, 8);
+    final c3 = Offset(7 * rWithFactor, 8);
+
+    canvas.drawCircle(c1, rWithFactor, circlePaint1);
+    canvas.drawCircle(c2, rWithFactor, circlePaint2);
+    canvas.drawCircle(c3, rWithFactor, circlePaint3);
+  }
+
+  @override
+  bool shouldRepaint(LrcTransitionPainter oldDelegate) => false;
+
+  @override
+  bool shouldRebuildSemantics(LrcTransitionPainter oldDelegate) => false;
+}
+
+class LrcTransitionTileController extends ChangeNotifier {
+  final LrcLine line;
+
+  final playService = PlayService.instance;
+
+  double progress = 0;
+  late final StreamSubscription positionStreamSub;
+
+  double sizeFactor = 0;
+  double k = 1;
+  late final Ticker factorTicker;
+
+  LrcTransitionTileController(this.line) {
+    positionStreamSub = playService.positionStream.listen(_updateProgress);
+    factorTicker = Ticker((elapsed) {
+      sizeFactor += k * 1 / 180;
+      if (sizeFactor > 1) {
+        k = -1;
+        sizeFactor = 1;
+      } else if (sizeFactor < 0) {
+        k = 1;
+        sizeFactor = 0;
+      }
+      notifyListeners();
+    });
+    factorTicker.start();
+  }
+
+  void _updateProgress(double position) {
+    final sinceStart = position * 1000 - line.start.inMilliseconds;
+    progress = max(sinceStart, 0) / line.length.inMilliseconds;
+    notifyListeners();
+
+    if (progress >= 1) {
+      dispose();
+    }
+  }
+
+  @override
+  void dispose() {
+    positionStreamSub.cancel();
+    factorTicker.dispose();
+    super.dispose();
+  }
+}
--- lib/page/now_playing_page/component/vertical_lyric_view.dart
+++ lib/page/now_playing_page/component/vertical_lyric_view.dart
@@ -2,11 +2,11 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:coriander_player/lyric/lrc.dart';
+import 'package:coriander_player/lyric/lyric.dart';
+import 'package:coriander_player/page/now_playing_page/component/lyric_source_label.dart';
+import 'package:coriander_player/page/now_playing_page/component/lyric_view_tile.dart';
 import 'package:coriander_player/play_service.dart';
-import 'package:coriander_player/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
-import 'package:provider/provider.dart';
 
 class VerticalLyricView extends StatefulWidget {
   const VerticalLyricView({super.key});
@@ -23,8 +23,10 @@ class _VerticalLyricViewState extends State<VerticalLyricView> {
   /// 隐藏滚动条
   final scrollBehavier = const ScrollBehavior().copyWith(scrollbars: false);
 
-  Lrc? lyric;
-  late List<_LyricViewTile> lyricTiles;
+  Lyric? lyric;
+  List<LyricViewTile> lyricTiles = [
+    LyricViewTile(line: LrcLine.blankLine, opacity: 1.0)
+  ];
 
   /// 用来定位到当前歌词
   final currentLyricTileKey = GlobalKey();
@@ -35,21 +37,16 @@ class _VerticalLyricViewState extends State<VerticalLyricView> {
 
     _initLyricView();
 
-    playService.addListener(_updateLyric);
+    playService.currentLyric.addListener(_updateLyric);
     lyricLineStreamSubscription =
         playService.lyricLineStream.listen(_updateNextLyricLine);
   }
 
   /// 加载当前歌词页面，获取并滚动到当前歌词行的位置
   void _initLyricView() {
-    lyric = playService.nowPlayingLyric;
+    lyric = playService.currentLyric.value;
     if (lyric == null) {
-      lyricTiles = [
-        _LyricViewTile(
-          line: LrcLine.blankLine,
-          opacity: 1.0,
-        )
-      ];
+      lyricTiles = [LyricViewTile(line: LrcLine.blankLine, opacity: 1.0)];
     } else {
       final next = lyric!.lines.indexWhere(
         (element) => element.start.inMilliseconds / 1000 > playService.position,
@@ -83,41 +80,41 @@ class _VerticalLyricViewState extends State<VerticalLyricView> {
 
   /// 当前歌词行100%透明度，上一句和下一句55%，其余10%
   /// 把[currentLyricTileKey]绑在当前歌词行上
-  List<_LyricViewTile> _generateLyricTiles(int lyricLine) {
+  List<LyricViewTile> _generateLyricTiles(int mainLine) {
     return List.generate(
       lyric!.lines.length,
       (i) {
-        if (lyricLine >= 2 && i <= lyricLine - 2) {
-          return _LyricViewTile(
-            line: lyric!.lines[i] as LrcLine,
+        if (mainLine >= 2 && i <= mainLine - 2) {
+          return LyricViewTile(
+            line: lyric!.lines[i],
             opacity: 0.10,
             onTap: () => _seekToLyricLine(i),
           );
         }
-        if (lyricLine >= 1 && i == lyricLine - 1) {
-          return _LyricViewTile(
-            line: lyric!.lines[i] as LrcLine,
+        if (mainLine >= 1 && i == mainLine - 1) {
+          return LyricViewTile(
+            line: lyric!.lines[i],
             opacity: 0.55,
             onTap: () => _seekToLyricLine(i),
           );
         }
-        if (lyricLine < lyric!.lines.length - 1 && i == lyricLine + 1) {
-          return _LyricViewTile(
-            line: lyric!.lines[i] as LrcLine,
+        if (mainLine < lyric!.lines.length - 1 && i == mainLine + 1) {
+          return LyricViewTile(
+            line: lyric!.lines[i],
             opacity: 0.55,
             onTap: () => _seekToLyricLine(i),
           );
         }
-        if (lyricLine < lyric!.lines.length - 2 && i >= lyricLine + 2) {
-          return _LyricViewTile(
-            line: lyric!.lines[i] as LrcLine,
+        if (mainLine < lyric!.lines.length - 2 && i >= mainLine + 2) {
+          return LyricViewTile(
+            line: lyric!.lines[i],
             opacity: 0.10,
             onTap: () => _seekToLyricLine(i),
           );
         }
-        return _LyricViewTile(
+        return LyricViewTile(
           key: currentLyricTileKey,
-          line: lyric!.lines[i] as LrcLine,
+          line: lyric!.lines[i],
           opacity: 1.0,
           onTap: () => _seekToLyricLine(i),
         );
@@ -127,10 +124,10 @@ class _VerticalLyricViewState extends State<VerticalLyricView> {
 
   /// 重新获取歌词
   void _updateLyric() {
-    lyric = playService.nowPlayingLyric;
+    lyric = playService.currentLyric.value;
     if (lyric == null) {
       lyricTiles = [
-        _LyricViewTile(
+        LyricViewTile(
           line: LrcLine.blankLine,
           opacity: 1.0,
           onTap: () {},
@@ -192,222 +189,7 @@ class _VerticalLyricViewState extends State<VerticalLyricView> {
   void dispose() {
     super.dispose();
     lyricLineStreamSubscription.cancel();
-    playService.removeListener(_updateLyric);
+    playService.currentLyric.removeListener(_updateLyric);
     scrollController.dispose();
-  }
-}
-
-class LyricSourceLabel extends StatelessWidget {
-  const LyricSourceLabel({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Provider.of<ThemeProvider>(context);
-    final playService = Provider.of<PlayService>(context);
-    final lyric = playService.nowPlayingLyric;
-
-    return Align(
-      alignment: Alignment.bottomRight,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(4.0),
-            border: Border.all(color: theme.palette.outline),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: Text(
-              lyric == null
-                  ? "无"
-                  : lyric.lines.isEmpty
-                      ? "歌词为空"
-                      : lyric.source.name,
-              style: TextStyle(
-                fontSize: 12,
-                color: theme.palette.onSecondaryContainer,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _LyricViewTile extends StatelessWidget {
-  const _LyricViewTile(
-      {super.key, required this.line, required this.opacity, this.onTap});
-
-  final LrcLine line;
-  final double opacity;
-  final void Function()? onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Provider.of<ThemeProvider>(context);
-
-    if (line.isBlank) {
-      if (line.length > const Duration(seconds: 5) && opacity == 1.0) {
-        return _LyricCountDownTile(line: line);
-      } else {
-        return const SizedBox();
-      }
-    }
-
-    final texts = line.content.split("┃");
-    final List<Text> textTiles = [
-      Text(
-        texts.first,
-        style: TextStyle(
-          color: theme.palette.onSecondaryContainer,
-          fontSize: 22.0,
-          fontWeight: FontWeight.w600,
-        ),
-      ),
-    ];
-    for (var i = 1; i < texts.length; i++) {
-      textTiles.add(Text(
-        texts[i],
-        style: TextStyle(
-          color: theme.palette.onSecondaryContainer,
-          fontSize: 18.0,
-        ),
-      ));
-    }
-    return Align(
-      alignment: Alignment.centerLeft,
-      child: Opacity(
-        opacity: opacity,
-        child: InkWell(
-          onTap: onTap,
-          borderRadius: BorderRadius.circular(12.0),
-          hoverColor: theme.palette.onSecondaryContainer.withOpacity(0.08),
-          highlightColor: theme.palette.onSecondaryContainer.withOpacity(0.12),
-          splashColor: theme.palette.onSecondaryContainer.withOpacity(0.12),
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: textTiles,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _LyricCountDownTile extends StatelessWidget {
-  final LrcLine line;
-  const _LyricCountDownTile({required this.line});
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Provider.of<ThemeProvider>(context);
-
-    return Align(
-      alignment: Alignment.centerLeft,
-      child: SizedBox(
-        height: 40.0,
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(12, 18, 12, 6),
-          child: CustomPaint(
-            painter: CountDownTilePainter(
-              theme,
-              CountDownTileController(line),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class CountDownTilePainter extends CustomPainter {
-  final ThemeProvider theme;
-  final CountDownTileController controller;
-
-  final Paint foregroundPaint1 = Paint();
-  final Paint foregroundPaint2 = Paint();
-  final Paint foregroundPaint3 = Paint();
-
-  final double radius = 6;
-
-  CountDownTilePainter(this.theme, this.controller)
-      : super(repaint: controller);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    foregroundPaint1.color = theme.palette.onSecondaryContainer.withOpacity(
-      0.05 + min(controller.progress * 3, 1) * 0.95,
-    );
-    foregroundPaint2.color = theme.palette.onSecondaryContainer.withOpacity(
-      0.05 + min(max(controller.progress - 1 / 3, 0) * 3, 1) * 0.95,
-    );
-    foregroundPaint3.color = theme.palette.onSecondaryContainer.withOpacity(
-      0.05 + min(max(controller.progress - 2 / 3, 0) * 3, 1) * 0.95,
-    );
-
-    final rWithFactor = radius + controller.sizeFactor;
-    final c1 = Offset(rWithFactor, 8);
-    final c2 = Offset(4 * rWithFactor, 8);
-    final c3 = Offset(7 * rWithFactor, 8);
-
-    canvas.drawCircle(c1, rWithFactor, foregroundPaint1);
-    canvas.drawCircle(c2, rWithFactor, foregroundPaint2);
-    canvas.drawCircle(c3, rWithFactor, foregroundPaint3);
-  }
-
-  @override
-  bool shouldRepaint(CountDownTilePainter oldDelegate) => false;
-
-  @override
-  bool shouldRebuildSemantics(CountDownTilePainter oldDelegate) => false;
-}
-
-class CountDownTileController extends ChangeNotifier {
-  final LrcLine line;
-
-  final playService = PlayService.instance;
-
-  double progress = 0;
-  late final StreamSubscription positionStreamSub;
-
-  double sizeFactor = 0;
-  double k = 1;
-  late final Ticker factorTicker;
-
-  CountDownTileController(this.line) {
-    positionStreamSub = playService.positionStream.listen(_updateProgress);
-    factorTicker = Ticker((elapsed) {
-      sizeFactor += k * 1 / 180;
-      if (sizeFactor > 1) {
-        k = -1;
-        sizeFactor = 1;
-      } else if (sizeFactor < 0) {
-        k = 1;
-        sizeFactor = 0;
-      }
-      notifyListeners();
-    });
-    factorTicker.start();
-  }
-
-  void _updateProgress(double position) {
-    final sinceStart = position * 1000 - line.start.inMilliseconds;
-    progress = max(sinceStart, 0) / line.length.inMilliseconds;
-    notifyListeners();
-
-    if (progress >= 1) {
-      dispose();
-    }
-  }
-
-  @override
-  void dispose() {
-    positionStreamSub.cancel();
-    factorTicker.dispose();
-    super.dispose();
   }
 }
