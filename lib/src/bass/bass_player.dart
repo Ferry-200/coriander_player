863650f82a4adfc9ffe6bb3b3718583306c58d88
Ferry-200 <biyanci1@outlook.com>
2024-06-21 20:10:28 +0800

    更正错误处理

--- lib/src/bass/bass_player.dart
+++ lib/src/bass/bass_player.dart
@@ -81,7 +81,7 @@ class BassPlayer {
     return volDsp.value;
   }
 
-  /// update every 200ms
+  /// update every 33ms
   Stream<double> get positionStream => _positionStreamController.stream;
 
   Stream<PlayerState> get playerStateStream =>
@@ -109,31 +109,32 @@ class BassPlayer {
         path.join(path.dirname(Platform.resolvedExecutable), 'bass.dll');
     _dyLib = ffi.DynamicLibrary.open(dyLibPath);
     _bass = Bass(_dyLib);
-    _bass.BASS_Init(1, 48000, 0, ffi.nullptr, ffi.nullptr);
 
-    switch (_bass.BASS_ErrorGetCode()) {
-      case BASS_ERROR_DEVICE:
-        throw const FormatException("device is invalid.");
-      case BASS_ERROR_NOTAVAIL:
-        throw const FormatException(
-            "The BASS_DEVICE_REINIT flag cannot be used when device is -1. Use the real device number instead.");
-      case BASS_ERROR_ALREADY:
-        throw const FormatException(
-            "The device has already been initialized. The BASS_DEVICE_REINIT flag can be used to request reinitialization.");
-      case BASS_ERROR_ILLPARAM:
-        throw const FormatException("win is not a valid window handle.");
-      case BASS_ERROR_DRIVER:
-        throw const FormatException("There is no available device driver.");
-      case BASS_ERROR_BUSY:
-        throw const FormatException(
-            "Something else has exclusive use of the device.");
-      case BASS_ERROR_FORMAT:
-        throw const FormatException(
-            "The specified format is not supported by the device. Try changing the freq parameter.");
-      case BASS_ERROR_MEM:
-        throw const FormatException("There is insufficient memory.");
-      case BASS_ERROR_UNKNOWN:
-        throw const FormatException("Some other mystery problem!");
+    if (_bass.BASS_Init(1, 48000, 0, ffi.nullptr, ffi.nullptr) == 0) {
+      switch (_bass.BASS_ErrorGetCode()) {
+        case BASS_ERROR_DEVICE:
+          throw const FormatException("device is invalid.");
+        case BASS_ERROR_NOTAVAIL:
+          throw const FormatException(
+              "The BASS_DEVICE_REINIT flag cannot be used when device is -1. Use the real device number instead.");
+        case BASS_ERROR_ALREADY:
+          throw const FormatException(
+              "The device has already been initialized. The BASS_DEVICE_REINIT flag can be used to request reinitialization.");
+        case BASS_ERROR_ILLPARAM:
+          throw const FormatException("win is not a valid window handle.");
+        case BASS_ERROR_DRIVER:
+          throw const FormatException("There is no available device driver.");
+        case BASS_ERROR_BUSY:
+          throw const FormatException(
+              "Something else has exclusive use of the device.");
+        case BASS_ERROR_FORMAT:
+          throw const FormatException(
+              "The specified format is not supported by the device. Try changing the freq parameter.");
+        case BASS_ERROR_MEM:
+          throw const FormatException("There is insufficient memory.");
+        case BASS_ERROR_UNKNOWN:
+          throw const FormatException("Some other mystery problem!");
+      }
     }
 
     // load bassflac add-on to avoid using os codec
@@ -181,39 +182,47 @@ class BassPlayer {
 
     /// 设置 flags 为 BASS_UNICODE 才可以找到文件。
     _fstream = _bass.BASS_StreamCreateFile(
-        FALSE, pathPointer, 0, 0, BASS_UNICODE | BASS_SAMPLE_FLOAT);
+      FALSE,
+      pathPointer,
+      0,
+      0,
+      BASS_UNICODE | BASS_SAMPLE_FLOAT,
+    );
 
-    switch (_bass.BASS_ErrorGetCode()) {
-      case BASS_ERROR_INIT:
-        throw const FormatException(
-            "BASS_Init has not been successfully called.");
-      case BASS_ERROR_NOTAVAIL:
-        throw const FormatException(
-            "The BASS_STREAM_AUTOFREE flag cannot be combined with the BASS_STREAM_DECODE flag.");
-      case BASS_ERROR_ILLPARAM:
-        throw const FormatException(
-            "The length must be specified when streaming from memory.");
-      case BASS_ERROR_FILEOPEN:
-        throw const FormatException("The file could not be opened.");
-      case BASS_ERROR_FILEFORM:
-        throw const FormatException(
-            "The file's format is not recognised/supported.");
-      case BASS_ERROR_NOTAUDIO:
-        throw const FormatException(
-            "The file does not contain audio, or it also contains video and videos are disabled.");
-      case BASS_ERROR_CODEC:
-        throw const FormatException(
-            "The file uses a codec that is not available/supported. This can apply to WAV and AIFF files.");
-      case BASS_ERROR_FORMAT:
-        throw const FormatException("The sample format is not supported.");
-      case BASS_ERROR_SPEAKER:
-        throw const FormatException("The specified SPEAKER flags are invalid.");
-      case BASS_ERROR_MEM:
-        throw const FormatException("There is insufficient memory.");
-      case BASS_ERROR_NO3D:
-        throw const FormatException("Could not initialize 3D support.");
-      case BASS_ERROR_UNKNOWN:
-        throw const FormatException("Some other mystery problem!");
+    if (_fstream == 0) {
+      switch (_bass.BASS_ErrorGetCode()) {
+        case BASS_ERROR_INIT:
+          throw const FormatException(
+              "BASS_Init has not been successfully called.");
+        case BASS_ERROR_NOTAVAIL:
+          throw const FormatException(
+              "The BASS_STREAM_AUTOFREE flag cannot be combined with the BASS_STREAM_DECODE flag.");
+        case BASS_ERROR_ILLPARAM:
+          throw const FormatException(
+              "The length must be specified when streaming from memory.");
+        case BASS_ERROR_FILEOPEN:
+          throw const FormatException("The file could not be opened.");
+        case BASS_ERROR_FILEFORM:
+          throw const FormatException(
+              "The file's format is not recognised/supported.");
+        case BASS_ERROR_NOTAUDIO:
+          throw const FormatException(
+              "The file does not contain audio, or it also contains video and videos are disabled.");
+        case BASS_ERROR_CODEC:
+          throw const FormatException(
+              "The file uses a codec that is not available/supported. This can apply to WAV and AIFF files.");
+        case BASS_ERROR_FORMAT:
+          throw const FormatException("The sample format is not supported.");
+        case BASS_ERROR_SPEAKER:
+          throw const FormatException(
+              "The specified SPEAKER flags are invalid.");
+        case BASS_ERROR_MEM:
+          throw const FormatException("There is insufficient memory.");
+        case BASS_ERROR_NO3D:
+          throw const FormatException("Could not initialize 3D support.");
+        case BASS_ERROR_UNKNOWN:
+          throw const FormatException("Some other mystery problem!");
+      }
     }
   }
 
@@ -221,15 +230,20 @@ class BassPlayer {
   void setVolumeDsp(double volume) {
     if (_fstream == null) return;
 
-    _bass.BASS_ChannelSetAttribute(_fstream!, BASS_ATTRIB_VOLDSP, volume);
-
-    switch (_bass.BASS_ErrorGetCode()) {
-      case BASS_ERROR_HANDLE:
-        throw const FormatException("handle is not a valid channel.");
-      case BASS_ERROR_ILLTYPE:
-        throw const FormatException("attrib is not valid.");
-      case BASS_ERROR_ILLPARAM:
-        throw const FormatException("value is not valid.");
+    if (_bass.BASS_ChannelSetAttribute(
+          _fstream!,
+          BASS_ATTRIB_VOLDSP,
+          volume,
+        ) ==
+        0) {
+      switch (_bass.BASS_ErrorGetCode()) {
+        case BASS_ERROR_HANDLE:
+          throw const FormatException("handle is not a valid channel.");
+        case BASS_ERROR_ILLTYPE:
+          throw const FormatException("attrib is not valid.");
+        case BASS_ERROR_ILLPARAM:
+          throw const FormatException("value is not valid.");
+      }
     }
   }
 
@@ -239,19 +253,20 @@ class BassPlayer {
   void start() {
     if (_fstream == null) return;
 
-    _bass.BASS_ChannelStart(_fstream!);
-    _playerStateStreamController.add(PlayerState.playing);
-    switch (_bass.BASS_ErrorGetCode()) {
-      case BASS_ERROR_HANDLE:
-        throw const FormatException("handle is not a valid channel.");
-      case BASS_ERROR_DECODE:
-        throw const FormatException(
-            "handle is a decoding channel, so cannot be played.");
-      case BASS_ERROR_START:
-        throw const FormatException(
-            "The output is paused/stopped, use BASS_Start to start it.");
+    if (_bass.BASS_ChannelStart(_fstream!) == 0) {
+      switch (_bass.BASS_ErrorGetCode()) {
+        case BASS_ERROR_HANDLE:
+          throw const FormatException("handle is not a valid channel.");
+        case BASS_ERROR_DECODE:
+          throw const FormatException(
+              "handle is a decoding channel, so cannot be played.");
+        case BASS_ERROR_START:
+          throw const FormatException(
+              "The output is paused/stopped, use BASS_Start to start it.");
+      }
     }
 
+    _playerStateStreamController.add(PlayerState.playing);
     _positionUpdater = _getPositionUpdater();
   }
 
@@ -261,18 +276,19 @@ class BassPlayer {
   void pause() {
     if (_fstream == null) return;
 
-    _bass.BASS_ChannelPause(_fstream!);
-    _playerStateStreamController.add(PlayerState.paused);
-    switch (_bass.BASS_ErrorGetCode()) {
-      case BASS_ERROR_HANDLE:
-        throw const FormatException("handle is not a valid channel.");
-      case BASS_ERROR_DECODE:
-        throw const FormatException(
-            "handle is a decoding channel, so cannot be played or paused.");
-      case BASS_ERROR_NOPLAY:
-        throw const FormatException("The channel is not playing.");
+    if (_bass.BASS_ChannelPause(_fstream!) == 0) {
+      switch (_bass.BASS_ErrorGetCode()) {
+        case BASS_ERROR_HANDLE:
+          throw const FormatException("handle is not a valid channel.");
+        case BASS_ERROR_DECODE:
+          throw const FormatException(
+              "handle is a decoding channel, so cannot be played or paused.");
+        case BASS_ERROR_NOPLAY:
+          throw const FormatException("The channel is not playing.");
+      }
     }
 
+    _playerStateStreamController.add(PlayerState.paused);
     _positionUpdater.cancel();
   }
 
@@ -282,13 +298,14 @@ class BassPlayer {
   void stop() {
     if (_fstream == null) return;
 
-    _bass.BASS_ChannelStop(_fstream!);
-    _playerStateStreamController.add(PlayerState.stopped);
-    switch (_bass.BASS_ErrorGetCode()) {
-      case BASS_ERROR_HANDLE:
-        throw const FormatException("handle is not a valid channel.");
+    if (_bass.BASS_ChannelStop(_fstream!) == 0) {
+      switch (_bass.BASS_ErrorGetCode()) {
+        case BASS_ERROR_HANDLE:
+          throw const FormatException("handle is not a valid channel.");
+      }
     }
 
+    _playerStateStreamController.add(PlayerState.stopped);
     _positionUpdater.cancel();
   }
 
@@ -299,24 +316,26 @@ class BassPlayer {
   void seek(double position) {
     if (_fstream == null) return;
 
-    _bass.BASS_ChannelSetPosition(
-      _fstream!,
-      _bass.BASS_ChannelSeconds2Bytes(_fstream!, position),
-      BASS_POS_BYTE,
-    );
-    switch (_bass.BASS_ErrorGetCode()) {
-      case BASS_ERROR_HANDLE:
-        throw const FormatException("handle is not a valid channel.");
-      case BASS_ERROR_NOTFILE:
-        throw const FormatException("The stream is not a file stream.");
-      case BASS_ERROR_POSITION:
-        throw const FormatException(
-            "The requested position is invalid, eg. it is beyond the end or the download has not yet reached it.");
-      case BASS_ERROR_NOTAVAIL:
-        throw const FormatException(
-            "The requested mode is not available. Invalid flags are ignored and do not result in this error.");
-      case BASS_ERROR_UNKNOWN:
-        throw const FormatException("Some other mystery problem!");
+    if (_bass.BASS_ChannelSetPosition(
+          _fstream!,
+          _bass.BASS_ChannelSeconds2Bytes(_fstream!, position),
+          BASS_POS_BYTE,
+        ) ==
+        0) {
+      switch (_bass.BASS_ErrorGetCode()) {
+        case BASS_ERROR_HANDLE:
+          throw const FormatException("handle is not a valid channel.");
+        case BASS_ERROR_NOTFILE:
+          throw const FormatException("The stream is not a file stream.");
+        case BASS_ERROR_POSITION:
+          throw const FormatException(
+              "The requested position is invalid, eg. it is beyond the end or the download has not yet reached it.");
+        case BASS_ERROR_NOTAVAIL:
+          throw const FormatException(
+              "The requested mode is not available. Invalid flags are ignored and do not result in this error.");
+        case BASS_ERROR_UNKNOWN:
+          throw const FormatException("Some other mystery problem!");
+      }
     }
   }
 
@@ -327,13 +346,14 @@ class BassPlayer {
   void freeFStream() {
     if (_fstream == null) return;
 
-    _bass.BASS_StreamFree(_fstream!);
-    switch (_bass.BASS_ErrorGetCode()) {
-      case BASS_ERROR_HANDLE:
-        throw const FormatException("handle is not valid.");
-      case BASS_ERROR_NOTAVAIL:
-        throw const FormatException(
-            "Device streams (STREAMPROC_DEVICE) cannot be freed.");
+    if (_bass.BASS_StreamFree(_fstream!) == 0) {
+      switch (_bass.BASS_ErrorGetCode()) {
+        case BASS_ERROR_HANDLE:
+          throw const FormatException("handle is not valid.");
+        case BASS_ERROR_NOTAVAIL:
+          throw const FormatException(
+              "Device streams (STREAMPROC_DEVICE) cannot be freed.");
+      }
     }
   }
 
@@ -345,17 +365,18 @@ class BassPlayer {
     for (var hplugin in pluginHandles) {
       _bass.BASS_PluginFree(hplugin);
     }
-    _bass.BASS_Free();
-    switch (_bass.BASS_ErrorGetCode()) {
-      case BASS_ERROR_INIT:
-        throw const FormatException(
-            "BASS_Init has not been successfully called.");
-      case BASS_ERROR_BUSY:
-        throw const FormatException(
-            "The device is currently being reinitialized.");
+    if (_bass.BASS_Free() == 0) {
+      switch (_bass.BASS_ErrorGetCode()) {
+        case BASS_ERROR_INIT:
+          throw const FormatException(
+              "BASS_Init has not been successfully called.");
+        case BASS_ERROR_BUSY:
+          throw const FormatException(
+              "The device is currently being reinitialized.");
+      }
     }
-    _dyLib.close();
 
+    _dyLib.close();
     _playerStateStreamController.close();
     _positionStreamController.close();
     _positionUpdater.cancel();
