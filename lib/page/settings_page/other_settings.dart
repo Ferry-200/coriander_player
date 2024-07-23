4dcd8ebcedf9cb3e1935303721547f21de99c1ed
Ferry-200 <biyanci1@outlook.com>
2024-07-23 15:14:48 +0800

    新增 settings tile 组件

--- lib/page/settings_page/other_settings.dart
+++ lib/page/settings_page/other_settings.dart
@@ -1,6 +1,9 @@
 import 'package:coriander_player/app_settings.dart';
-import 'package:coriander_player/component/folder_tile.dart';
+import 'package:coriander_player/component/build_index_state_view.dart';
+import 'package:coriander_player/component/settings_tile.dart';
 import 'package:coriander_player/library/audio_library.dart';
+import 'package:coriander_player/library/playlist.dart';
+import 'package:coriander_player/lyric/lyric_source.dart';
 import 'package:coriander_player/src/rust/api/tag_reader.dart';
 import 'package:coriander_player/src/rust/api/utils.dart';
 import 'package:flutter/material.dart';
@@ -20,155 +23,183 @@ class _DefaultLyricSourceControlState extends State<DefaultLyricSourceControl> {
 
   @override
   Widget build(BuildContext context) {
-    final scheme = Theme.of(context).colorScheme;
+    return SettingsTile(
+      description: "首选歌词来源",
+      action: SegmentedButton<bool>(
+        showSelectedIcon: false,
+        segments: const [
+          ButtonSegment<bool>(
+            value: true,
+            icon: Icon(Symbols.cloud_off),
+            label: Text("本地"),
+          ),
+          ButtonSegment<bool>(
+            value: false,
+            icon: Icon(Symbols.cloud),
+            label: Text("在线"),
+          ),
+        ],
+        selected: {settings.localLyricFirst},
+        onSelectionChanged: (newSelection) async {
+          if (newSelection.first == settings.localLyricFirst) return;
 
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          "首选歌词来源",
-          style: TextStyle(color: scheme.onSurface, fontSize: 18.0),
-        ),
-        SegmentedButton<bool>(
-          showSelectedIcon: false,
-          segments: const [
-            ButtonSegment<bool>(
-              value: true,
-              icon: Icon(Symbols.cloud_off),
-              label: Text("本地"),
-            ),
-            ButtonSegment<bool>(
-              value: false,
-              icon: Icon(Symbols.cloud),
-              label: Text("在线"),
-            ),
-          ],
-          selected: {settings.localLyricFirst},
-          onSelectionChanged: (newSelection) async {
-            if (newSelection.first == settings.localLyricFirst) return;
-
-            setState(() {
-              settings.localLyricFirst = newSelection.first;
-            });
-            await settings.saveSettings();
-          },
-        ),
-      ],
+          setState(() {
+            settings.localLyricFirst = newSelection.first;
+          });
+          await settings.saveSettings();
+        },
+      ),
     );
   }
 }
 
-class AudioLibraryEditField extends StatefulWidget {
-  const AudioLibraryEditField({super.key});
+class AudioLibraryEditor extends StatelessWidget {
+  const AudioLibraryEditor({super.key});
 
   @override
-  State<AudioLibraryEditField> createState() => _AudioLibraryEditFieldState();
+  Widget build(BuildContext context) {
+    return SettingsTile(
+      description: "文件夹管理",
+      action: FilledButton.icon(
+        icon: const Icon(Symbols.folder),
+        label: const Text("文件夹管理"),
+        onPressed: () {
+          showDialog(
+            context: context,
+            barrierDismissible: false,
+            builder: (context) => const AudioLibraryEditorDialog(),
+          );
+        },
+      ),
+    );
+  }
 }
 
-class _AudioLibraryEditFieldState extends State<AudioLibraryEditField> {
+class AudioLibraryEditorDialog extends StatefulWidget {
+  const AudioLibraryEditorDialog({super.key});
+
+  @override
+  State<AudioLibraryEditorDialog> createState() =>
+      _AudioLibraryEditorDialogState();
+}
+
+class _AudioLibraryEditorDialogState extends State<AudioLibraryEditorDialog> {
+  final folders = List.generate(
+    AudioLibrary.instance.folders.length,
+    (i) => AudioLibrary.instance.folders[i].path,
+  );
+
+  final applicationSupportDirectory = getApplicationSupportDirectory();
+
+  bool editing = true;
+
   @override
   Widget build(BuildContext context) {
     final scheme = Theme.of(context).colorScheme;
-    final audioLibrary = AudioLibrary.instance;
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(bottom: 8.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Dialog(
+      insetPadding: EdgeInsets.zero,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: SizedBox(
+        height: 450.0,
+        width: 450.0,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                "文件夹管理",
-                style: TextStyle(color: scheme.onSurface, fontSize: 18.0),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: Text(
+                  "管理文件夹",
+                  style: TextStyle(
+                    color: scheme.onSurface,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
               ),
+              Expanded(
+                child: AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 150),
+                  child: editing
+                      ? ListView.builder(
+                          itemCount: folders.length,
+                          itemBuilder: (context, i) => ListTile(
+                            title: Text(folders[i], maxLines: 1),
+                            trailing: IconButton(
+                              color: scheme.error,
+                              onPressed: () {
+                                setState(() {
+                                  folders.removeAt(i);
+                                });
+                              },
+                              icon: const Icon(Symbols.delete),
+                            ),
+                          ),
+                        )
+                      : FutureBuilder(
+                          future: applicationSupportDirectory,
+                          builder: (context, snapshot) {
+                            if (snapshot.data == null) {
+                              return const SizedBox.shrink();
+                            }
+
+                            return BuildIndexStateView(
+                              indexPath: snapshot.data!,
+                              folders: folders,
+                              whenIndexBuilt: () {
+                                Future.wait([
+                                  AudioLibrary.initFromIndex(),
+                                  readPlaylists(),
+                                  readLyricSources(),
+                                ]).whenComplete(() {
+                                  Navigator.pop(context);
+                                });
+                              },
+                            );
+                          },
+                        ),
+                ),
+              ),
+              const SizedBox(height: 16.0),
               Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  IconButton(
+                  TextButton(
                     onPressed: () async {
-                      String? selectedDirectory = await pickSingleFolder();
-                      if (selectedDirectory == null) return;
+                      final path = await pickSingleFolder();
+                      if (path == null) return;
+
                       setState(() {
-                        audioLibrary.folders.add(
-                          AudioFolder([], selectedDirectory, 0),
-                        );
+                        folders.add(path);
                       });
                     },
-                    icon: const Icon(Symbols.add),
+                    child: const Text("添加"),
                   ),
                   const SizedBox(width: 8.0),
-                  const _BuildIndexButton(),
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text("取消"),
+                  ),
+                  const SizedBox(width: 8.0),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        editing = false;
+                      });
+                    },
+                    child: const Text("确定"),
+                  ),
                 ],
-              ),
+              )
             ],
           ),
         ),
-        ConstrainedBox(
-          constraints: const BoxConstraints(maxHeight: 250.0),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: List.generate(
-                audioLibrary.folders.length,
-                (i) => FolderTile(
-                  path: audioLibrary.folders[i].path,
-                  onDelete: () {
-                    setState(() {
-                      audioLibrary.folders.removeAt(i);
-                    });
-                  },
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class _BuildIndexButton extends StatefulWidget {
-  const _BuildIndexButton();
-
-  @override
-  State<_BuildIndexButton> createState() => __BuildIndexButtonState();
-}
-
-class __BuildIndexButtonState extends State<_BuildIndexButton> {
-  var isLoading = false;
-
-  void save() async {
-    final audioLibrary = AudioLibrary.instance;
-    setState(() {
-      isLoading = true;
-    });
-    final indexPath = (await getApplicationSupportDirectory()).path;
-    await buildIndexFromPaths(
-      paths: List.generate(
-        audioLibrary.folders.length,
-        (i) => audioLibrary.folders[i].path,
       ),
-      indexPath: indexPath,
-    );
-    await AudioLibrary.initFromIndex();
-    setState(() {
-      isLoading = false;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return IconButton(
-      onPressed: isLoading ? null : save,
-      icon: isLoading
-          ? const SizedBox(
-              width: 18,
-              height: 18,
-              child: CircularProgressIndicator(),
-            )
-          : const Icon(Symbols.refresh),
     );
   }
 }
