b2057dda2574d41beb22e31f16fb73a54576c8f1
周诚 <zhoucheng133@outlook.com>
2024-08-29 01:54:15 +0800

    修复打开时黑屏一下的问题

--- windows/runner/flutter_window.cpp
+++ windows/runner/flutter_window.cpp
@@ -28,7 +28,7 @@ bool FlutterWindow::OnCreate() {
   SetChildContent(flutter_controller_->view()->GetNativeWindow());
 
   flutter_controller_->engine()->SetNextFrameCallback([&]() {
-    this->Show();
+    // this->Show();
   });
 
   // Flutter can complete the first frame before the "show window" callback is
